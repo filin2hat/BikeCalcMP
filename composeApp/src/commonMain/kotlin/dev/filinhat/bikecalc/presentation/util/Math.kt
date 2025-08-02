ae7ab0dc3d83aa9bda61d2eb875790813bcd8741
Dmitriy Biryulin <filin2hat@yandex.ru>
2025-08-02 16:07:03 +0300

    Функциональность: Добавлены функции для преобразования единиц измерения
    В этом коммите добавлен новый файл `Math.kt`, содержащий функции-расширения для преобразования значений между различными единицами измерения:
    - Давление: из бар в кПа и psi.
    - Масса: из фунтов в килограммы и наоборот.

---
+++ composeApp/src/commonMain/kotlin/dev/filinhat/bikecalc/presentation/util/Math.kt
@@ -0,0 +1,25 @@
+package dev.filinhat.bikecalc.presentation.util
+
+/**
+ * Преобразует значение давления из бар в килопаскали (кПа).
+ * @return Значение давления в кПа.
+ */
+fun Double.barToKPa(): Double = this * 100
+
+/**
+ * Преобразует значение давления из бар в фунты на квадратный дюйм (psi).
+ * @return Значение давления в psi.
+ */
+fun Double.barToPsi(): Double = this * 14.5038
+
+/**
+ * Преобразует значение массы из фунтов (lbs) в килограммы (кг).
+ * @return Значение массы в кг.
+ */
+fun Double.lbsToKg(): Double = this / 2.20462
+
+/**
+ * Преобразует значение массы из килограммов (кг) в фунты (lbs).
+ * @return Значение массы в фунтах.
+ */
+fun Double.kgToLbs(): Double = this * 2.20462
