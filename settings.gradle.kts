ad77ecd61667e908c604515cca5d018bf920ceda
filin2hat <filin2hat@gmail.com>
2026-02-21 08:14:52 +0300

    upd: kotlin, agp, ksp

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,9 @@ pluginManagement {
         id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 
 dependencyResolutionManagement {
     repositories {
